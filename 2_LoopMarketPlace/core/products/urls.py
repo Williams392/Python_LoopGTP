3b531117f300497bad6408a9b3915201784d48bd
Williams392 <williamsvalle392@gmail.com>
2023-05-22 16:14:59 -0500

    api - prodcuts

--- 2_LoopMarketPlace/core/products/urls.py
+++ 2_LoopMarketPlace/core/products/urls.py
@@ -10,17 +10,3 @@ urlpatterns = [
     path('products/', ProductListView.as_view(), name='product-list'),
     path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
 ]
-
-
-""""
-.Primer paso:
-
-1. Registrar el settings en core ('products',).
-2. agregar la urls en la principal carpeta (core):
-   _ ath('products/', include('products.urls')),
-   
-3. crear el archivo (urls.py) en la carpeta "products"
-4. Crear la (urls) en dentro del archivo "urls.py".  
-5. el paso 5 esta en el (views.py)
-
-"""
